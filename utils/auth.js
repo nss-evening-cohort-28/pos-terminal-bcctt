2e71fed09d37b616da6434c862e94431dfd740f2
tderamus <deramust@gmail.com>
2024-09-15 21:03:24 -0500

    fixed bug in login and logout button to remove items from DOM and add greetng

--- utils/auth.js
+++ utils/auth.js
@@ -2,10 +2,6 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const signIn = () => {
-  const imageId = document.querySelector('#landing-image');
-  const loginBtn = document.querySelector('#login-form-container');
-  loginBtn.innerHTML = '';
-  imageId.parentNode.removeChild(imageId);
   const provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider);
 };
