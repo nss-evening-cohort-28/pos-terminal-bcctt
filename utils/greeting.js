ee71de70f7946c505edf46d3d818fb2508f2a9b3
dinnerdoggy <31261276+dinnerdoggy@users.noreply.github.com>
2024-09-16 11:21:41 -0500

    placed and styled the home screen buttons from homeScreen.js underneath the user greeting

--- utils/greeting.js
+++ utils/greeting.js
@@ -1,11 +1,13 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import homeScreen from '../pages/homeSreen';
 
 const greetLoggedInUser = () => {
   const userName = firebase.auth().currentUser.displayName;
   const userDisplay = document.querySelector('#main-container');
   const domString = `<div id="userGreeting">Welcome, ${userName}!</div>`;
   userDisplay.innerHTML = domString;
+  homeScreen();
 };
 
 export default greetLoggedInUser;
