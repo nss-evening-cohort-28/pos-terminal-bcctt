97131e2fc776b2136aeeef0815458695a90ec695
Casey <dinnerdoggy@gmail.com>
2024-09-16 19:29:59 -0500

    made the logo on the navbar link back to the home screen

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,11 +1,21 @@
 import { getOrders } from '../api/orderData';
+import homeScreen from '../pages/homeSreen';
 import viewOrders from '../pages/viewOrders';
+import clearDom from '../utils/clearDom';
+import greetLoggedInUser from '../utils/greeting';
 
 const navigationEvents = () => {
   document.querySelector('#app').addEventListener('click', (e) => {
     if (e.target.id.includes('viewOrder')) {
       console.warn('clicked View Orders!');
       getOrders().then((data) => viewOrders(data));
+    }
+
+    if (e.target.id.includes('homeLogo')) {
+      console.warn('clicked home!');
+      clearDom();
+      greetLoggedInUser();
+      homeScreen();
     }
   });
 };
