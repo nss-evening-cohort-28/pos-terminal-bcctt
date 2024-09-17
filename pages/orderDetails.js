467259d01c58054d0248b0994aad9c5ff779ee1e
dinnerdoggy <31261276+dinnerdoggy@users.noreply.github.com>
2024-09-17 15:24:35 -0500

    added buttons at the bottoms and added some padding on the item cards

--- pages/orderDetails.js
+++ pages/orderDetails.js
@@ -8,7 +8,7 @@ const orderDetails = (array) => {
   array.forEach((item) => {
     domString += `
     <div class="itemCard moreSpecific">
-        <div class="card-body">
+        <div class="card-body itemCard-body">
             <h5 class="card-title">${item.itemName}</h5>
             <h6 class="card-subtitle mb-2 text-body-secondary">${item.price}</h6>
             <a href="#" class="card-link">Edit Item</a>
@@ -19,6 +19,12 @@ const orderDetails = (array) => {
     sum += item.price;
   });
   const domStringSum = `<h1 id="orderSum">TOTAL: $${sum}</h1>`;
+  const btnGroup = `
+  <div id="detailsBtnGrp">
+    <button id="addItemBtn" type="button" class="btn btn-primary btn-lg">Add Item</button>
+    <button id="goToPaymentsBtn" type="button" class="btn btn-primary btn-lg">Go To Payment</button>
+  </div>`;
+  domString += btnGroup;
   renderToDOM('#main-container', domStringSum);
   renderToDOM('#itemCard-container', domString);
 };
