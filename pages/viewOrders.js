6a49ef1fc3460749bcd616d58f34bfb073984318
Casey <dinnerdoggy@gmail.com>
2024-09-18 21:55:19 -0500

    added ternaries to the viewOrders.js so that the dom will render the status and type of the orders

--- pages/viewOrders.js
+++ pages/viewOrders.js
@@ -9,10 +9,10 @@ const viewOrders = (array) => {
         <div class="card" style="width: 18rem;">
          <div class="card-body">
             <h5 class="card-title">${order.orderName}</h5>
-            <h6 class="card-subtitle mb-2 text-body-secondary">${order.status}</h6>
+            <h6 class="card-subtitle mb-2 text-body-secondary order-status">${order.status === true ? 'Open' : 'Closed'}</h6>
             <p class="card-text">${order.phoneNum}</p>
             <p class="card-text">${order.email}</p>
-            <p class="card-text">${order.type}</p>
+            <p class="card-text">${order.type === true ? 'Call in Order' : 'Walk-in Order'}</p>
             <a id="orderDetails" href="#" class="card-link">Details</a>
             <a id="update-order-btn--${order.firebaseKey}" href="#" class="card-link">Edit</a>
             <a id="delete-order-btn--${order.firebaseKey}" href="#" class="card-link">Delete</a>
